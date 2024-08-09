2b9f6ba54df6cce5e9186668b11c2f0c9310e986
zodiac1111 <zodiac8765@gmail.com>
2024-08-09 22:11:12 +0800

    format

--- src/ExcelHandler.ts
+++ src/ExcelHandler.ts
@@ -1,11 +1,11 @@
 import * as fs from 'fs';
 
 
-export function activate_winax(){
+export function activate_winax() {
     var winax = require('winax'); // required to define ActiveXObject
 }
 
-enum SourceType { Excel, M};
+enum SourceType { Excel, M };
 
 interface IDisposable {
     dispose(): void;
@@ -15,16 +15,16 @@ export class ExcelRegistry implements IDisposable {
     excel: Excel.Application | undefined;
     logger: (arg0: string) => void;
 
-    constructor(logger = function(msg : string) {} ){
+    constructor(logger = function (msg: string) { }) {
         this.logger = logger;
     }
 
-    isInitialized() : boolean {
+    isInitialized(): boolean {
         return this.excel !== undefined;
     }
 
-    getExcel() : Excel.Application {
-        if ( !this.excel || !this.excel.Workbooks){
+    getExcel(): Excel.Application {
+        if (!this.excel || !this.excel.Workbooks) {
             this.logger("Create new Excel application");
             this.excel = new ActiveXObject('Excel.Application');
             (this.excel as any).ShowStartupDialog = false;
@@ -34,17 +34,17 @@ export class ExcelRegistry implements IDisposable {
     }
 
     dispose(): void {
-        if (this.isInitialized() && this.getExcel().Workbooks.Count === 0){
+        if (this.isInitialized() && this.getExcel().Workbooks.Count === 0) {
             this.getExcel().Quit();
         }
     }
 
-    __getWorkbookFromCache(filename : string) : Excel.Workbook | undefined {
-        if (this.isInitialized()){
+    __getWorkbookFromCache(filename: string): Excel.Workbook | undefined {
+        if (this.isInitialized()) {
             let excel = this.getExcel();
-            for(let i=1; i<=excel.Workbooks.Count;i++){
+            for (let i = 1; i <= excel.Workbooks.Count; i++) {
                 let wb = excel.Workbooks.Item(i);
-                if (wb.FullName === filename){
+                if (wb.FullName === filename) {
                     this.logger("Workbook is already open. Retrieve it.");
                     return wb;
                 }
@@ -53,24 +53,24 @@ export class ExcelRegistry implements IDisposable {
         return undefined;
     }
 
-    open(filename : string) : Excel.Workbook {
-        let wb : Excel.Workbook | undefined = this.__getWorkbookFromCache(filename);
-        if (!wb){
+    open(filename: string): Excel.Workbook {
+        let wb: Excel.Workbook | undefined = this.__getWorkbookFromCache(filename);
+        if (!wb) {
             this.logger("Open workbook with Excel.")
             wb = this.getExcel().Workbooks.Open(filename);
         }
         return wb;
     }
 
-    close(filename : string, saveChanges : boolean) : void {
-        let wb : Excel.Workbook | undefined = this.__getWorkbookFromCache(filename);
+    close(filename: string, saveChanges: boolean): void {
+        let wb: Excel.Workbook | undefined = this.__getWorkbookFromCache(filename);
         if (wb) {
             this.logger("Close workbook " + filename);
-            wb.Close(saveChanges);    
+            wb.Close(saveChanges);
             wb = undefined;
         }
 
-        if (this.excel && this.excel.Workbooks.Count !== undefined && this.excel.Workbooks.Count === 0){
+        if (this.excel && this.excel.Workbooks.Count !== undefined && this.excel.Workbooks.Count === 0) {
             this.logger("All workbooks are closed. Close Excel also.");
             this.getExcel().DisplayAlerts = false;
             this.getExcel().Quit();
@@ -82,16 +82,16 @@ export class ExcelRegistry implements IDisposable {
 
 
 export class PowerQueryMCodeReader implements IDisposable {
-    queries : Map<string, string>;
+    queries: Map<string, string>;
     excelFileName!: string;
     pqmFileName: string;
-    sourceType : SourceType;
-    excelRegistry : ExcelRegistry;
-    readonly delimiter1 : string = "//######";
-    readonly delimiter2 : string = "## This is delimiter. Dont remove it\n";
+    sourceType: SourceType;
+    excelRegistry: ExcelRegistry;
+    readonly delimiter1: string = "//######";
+    readonly delimiter2: string = "## This is delimiter. Dont remove it\n";
 
-    constructor(fileName : string, excelRegistry : ExcelRegistry){
-        if (! fs.existsSync(fileName)){
+    constructor(fileName: string, excelRegistry: ExcelRegistry) {
+        if (!fs.existsSync(fileName)) {
             throw new Error("File not found ${fileName}");
         }
         this.excelRegistry = excelRegistry;
@@ -100,18 +100,18 @@ export class PowerQueryMCodeReader implements IDisposable {
         let xls_regexp = new RegExp("\\.xls.$");
         let m_regexp = new RegExp("\\.m$");
 
-        if (fileName.toLowerCase().match(xls_regexp)){
+        if (fileName.toLowerCase().match(xls_regexp)) {
             this.sourceType = SourceType.Excel;
             this.pqmFileName = fileName.toLowerCase().replace(xls_regexp, ".m");
             this.excelFileName = fileName
-        } else if (fileName.toLowerCase().match(m_regexp)){
+        } else if (fileName.toLowerCase().match(m_regexp)) {
             this.sourceType = SourceType.M;
             this.pqmFileName = fileName;
             let xlsXname = fileName.toLowerCase().replace(m_regexp, ".xlsx");
             let xlsMname = fileName.toLowerCase().replace(m_regexp, ".xlsm");
             if (fs.existsSync(xlsMname) && fs.existsSync(xlsXname)) {
                 throw new Error("xlsX and xlsM files with the same names " +
-                                "exist simultaniously. I dont know which to update");
+                    "exist simultaniously. I dont know which to update");
             } else if (fs.existsSync(xlsXname)) {
                 this.excelFileName = xlsXname;
             } else if (fs.existsSync(xlsMname)) {
@@ -128,12 +128,12 @@ export class PowerQueryMCodeReader implements IDisposable {
 
     importFromExcel(): void {
         console.log("Initalize from Excel file");
-        let workbook : any = this.excelRegistry.open(this.excelFileName);
+        let workbook: any = this.excelRegistry.open(this.excelFileName);
         let queries = workbook["Queries"];
-        if (queries === undefined){
+        if (queries === undefined) {
             throw new Error("Queries attribute of Excel Workbook is undefined. It could happen due to raise of Activation window on Excel startup. Check Excel and try again.");
         }
-        for(let i=1; i<=queries.Count; i++){
+        for (let i = 1; i <= queries.Count; i++) {
             this.queries.set(queries.Item(i).Name, queries.Item(i).Formula);
         }
         console.log(this.queries.size + " queries imported");
@@ -142,7 +142,7 @@ export class PowerQueryMCodeReader implements IDisposable {
     exportToFile(): void {
         console.log("Save to file");
         let buffer: string[] = [];
-        for(let [name, query] of this.queries){
+        for (let [name, query] of this.queries) {
             let newLine = this.delimiter1 + name + this.delimiter2 + query + "\n\n";
             buffer.push(newLine);
         }
@@ -156,31 +156,31 @@ export class PowerQueryMCodeReader implements IDisposable {
         console.log("Read " + queries.length.toString() + " queries");
         this.queries = new Map();
         let delimiter = this.delimiter2.trim();
-        for(let queryWithName of queries){
+        for (let queryWithName of queries) {
             let del2pos = queryWithName.indexOf(delimiter);
-            if (del2pos == -1){
+            if (del2pos == -1) {
                 continue;
-            }          
-            let name : string = queryWithName.substring(0, del2pos).trim();
-            let query : string=  queryWithName.substring(delimiter.length + del2pos, queryWithName.length).trim();
+            }
+            let name: string = queryWithName.substring(0, del2pos).trim();
+            let query: string = queryWithName.substring(delimiter.length + del2pos, queryWithName.length).trim();
             this.queries.set(name, query.trim());
         }
     }
 
-    exportToExcel() : void {
+    exportToExcel(): void {
         console.log("Save to Excel file");
-        let workbook : any = this.excelRegistry.open(this.excelFileName);
+        let workbook: any = this.excelRegistry.open(this.excelFileName);
         let excelQueries = workbook["Queries"];
-        if (excelQueries === undefined){
+        if (excelQueries === undefined) {
             throw new Error("Worbook.Queries is undefined. Unable to import. This could be due to Excel pop-up windows.");
         }
         // make a copy of a queries map
-        let queriesCopy : Map<string, string> = new Map(this.queries);
+        let queriesCopy: Map<string, string> = new Map(this.queries);
 
-        for(let i = 1; i <= excelQueries.Count; i++){
+        for (let i = 1; i <= excelQueries.Count; i++) {
             let item = excelQueries.Item(i);
-            let name = item.Name; 
-            if (queriesCopy.has(name)){
+            let name = item.Name;
+            if (queriesCopy.has(name)) {
                 item.Formula = queriesCopy.get(name);
                 queriesCopy.delete(name);
             } else {
@@ -190,12 +190,12 @@ export class PowerQueryMCodeReader implements IDisposable {
             }
         }
         // now I need to create new items for ones thar were not present in Excel
-        for(let [name, formula] of queriesCopy){
+        for (let [name, formula] of queriesCopy) {
             excelQueries.Add(name, formula);
         }
     }
 }
 
-if (require.main === module){
+if (require.main === module) {
     // pass
 }
